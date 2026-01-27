be964b7ef0f198f4bee5a9348cb566754e803eb1
theHrDev <hamzatrukayat2822@gmail.com>
2026-01-27 10:16:42 +0000

    updated the git script to check whether git has been installed previously

--- git.sh
+++ git.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 read -p "Enter the commit message: " message
 read -p "Enter the branch name :" branch
-sudo dnf install git -y
 
+package=git
+
+if command -v $package > /dev/null  2<&1; then
+	echo "git installed,proceeding to push"
+
+else
+	echo "$package has not been installed, installin..."
+	
+        sudo dnf install $package -y >/dev/null 2<&1
+
+fi
 git add $1
 
 git commit -m "$message"
