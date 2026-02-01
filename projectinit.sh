b0d3ce6e2144a52456517327ed24ee01401cbee8
theHrDev <hamzatrukayat2822@gmail.com>
2026-02-01 08:15:40 +0000

    Added a script for creating a folder

---
+++ projectinit.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+read -p "Enter the name for the folder: " folder_name
+
+if [ -d "$folder_name" ]; then
+	echo "$folder_name already exist"
+	read -p "Enter the name for the folder " folder_name
+else
+	mkdir "$folder_name"
+
+	if [ $? eq 0 ]; then
+		echo "$folder_name successfully created"
+
+	else
+		echo "Error while creating folder $folder_name"
+		exit 1
+
+mkdir $folder_name
+mkdir $folder_name/src
+mkdir $folder_name/tests
+mkdir $folder_name/docs
+touch $folder_name/README.md
+
+echo "successfully created folder $folder_name"
+
+
