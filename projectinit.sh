3613373501928d15fb7e59fb72810b42e66e86fc
theHrDev <hamzatrukayat2822@gmail.com>
2026-02-01 08:26:22 +0000

    Added string to the variable name

--- projectinit.sh
+++ projectinit.sh
@@ -9,21 +9,21 @@ else
 	mkdir "$folder_name"
 
 	if [ $? -eq 0 ]; then
-		echo "$folder_name successfully created"
+		echo "'$folder_name' successfully created"
 
 	else
-		echo "Error while creating folder $folder_name"
+		echo "Error while creating folder '$folder_name'"
 		exit 1
 	fi
 
 fi
 
-mkdir $folder_name
-mkdir $folder_name/src
-mkdir $folder_name/tests
-mkdir $folder_name/docs
-touch $folder_name/README.md
+mkdir "$folder_name"
+mkdir "$folder_name"/src
+mkdir "$folder_name"/tests
+mkdir "$folder_name"/docs
+touch "$folder_name"/README.md
 
-echo "successfully created folder $folder_name"
+echo "successfully created folder '$folder_name'"
 
 
