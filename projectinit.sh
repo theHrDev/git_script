4db4f2bd7f09f5f7ea6fb26976f42fcf27ab4115
theHrDev <hamzatrukayat2822@gmail.com>
2026-02-01 08:24:02 +0000

    fixed the if error

--- projectinit.sh
+++ projectinit.sh
@@ -4,16 +4,19 @@ read -p "Enter the name for the folder: " folder_name
 
 if [ -d "$folder_name" ]; then
 	echo "$folder_name already exist"
-	read -p "Enter the name for the folder " folder_name
+	#read -p "Enter the name for the folder " folder_name
 else
 	mkdir "$folder_name"
 
-	if [ $? eq 0 ]; then
+	if [ $? -eq 0 ]; then
 		echo "$folder_name successfully created"
 
 	else
 		echo "Error while creating folder $folder_name"
 		exit 1
+	fi
+
+fi
 
 mkdir $folder_name
 mkdir $folder_name/src
